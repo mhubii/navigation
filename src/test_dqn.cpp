10930a123b6182f4e6c319fd3ef1fcfef19c82a7
mhubii <m.huber_1994@hotmail.de>
2018-10-15 13:25:19 +0200

    removed caffe2 bloody edge

--- src/test_dqn.cpp
+++
@@ -1,6 +0,0 @@
-#include "dqn.h"
-
-int main(int argc, char **argv) {
-
-  return 0;
-}
--- utils/model_base.cpp
+++
@@ -1,9 +0,0 @@
-#include "model_base.h"
-
-namespace caffe2 {
-
-ModelBase::ModelBase(NetDef& init_net, NetDef& predict_net)
-	: init_(init_net), predict_(predict_net) {
-
-};
-} // caffe2
