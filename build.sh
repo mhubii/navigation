1c759052df65bc966d3eb28f4602c311b66b9a0b
mhubii <m.huber_1994@hotmail.de>
2018-10-19 15:49:03 +0200

    added build.sh

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Go to location of bash script.
+DIR=`dirname $(readlink -f \$0)`
+cd ${DIR}
+
+# Download libtorch.
+if ! [ -d "third_party/libtorch" ]; then
+
+	cd third_party
+	wget https://download.pytorch.org/libtorch/nightly/cu90/libtorch-shared-with-deps-latest.zip
+	unzip libtorch-shared-with-deps-latest.zip
+	rm libtorch-shared-with-deps-latest.zip
+	cd ..
+fi
+
+export CMAKE_PREFIX_PATH="${DIR}/third_party/libtorch"
+
+# Build the project.
+if ! [ -d "build" ]; then
+
+	mkdir build
+fi
+
+cd build
+cmake ..
+make -j8
+
+
+
