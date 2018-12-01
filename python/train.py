d74c221c475dc1ce760d6ec1ee2b0511f3e56edb
martin <m.huber_1994@hotmail.de>
2018-12-01 22:44:27 +0100

    added trace script module

--- python/train.py
+++ python/train.py
@@ -9,20 +9,10 @@ from model import StereoCNN
 import utils
 
 
-def train():
+def train(args):
     """
         Load data, build  and train model.
     """
-
-    # Get command line arguments.
-    parser = argparse.ArgumentParser(description='Behavioral Cloning Training Program')
-    parser.add_argument('-d', type=str,   help='data directory',   default='data', dest='data_dir')
-    parser.add_argument('-l', type=float, help='learning rate',    default=0.001,  dest='learning_rate')
-    parser.add_argument('-b', type=int,   help='batch size',       default=40,  dest='batch_size')
-    parser.add_argument('-e', type=int,   help='number of epochs', default=10,     dest='epochs')
-
-    args = parser.parse_args()
-
     # Load, pre-process and augment data.
     data_set = utils.DataSetGenerator(data_dir=args.data_dir,
                                       transform=transforms.Compose([
@@ -57,14 +47,33 @@ def train():
             # Save weights.
             if loss.data.item() < best_loss:
                 best_loss = loss.data.item()
-                torch.save(model.state_dict(), 'train.pth')
+                torch.save(model.state_dict(), 'trained.pt')
 
             if idx % 10 == 0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                       epoch, idx * len(img_left), len(data_loader.dataset),
-                      100. * idx / len(data_loader), loss.data.item()))
+                      100. * idx / len(data_loader), loss.data.item()))    
 
 
 if __name__ == '__main__':
+
+	# Get command line arguments.
+	parser = argparse.ArgumentParser(description='Behavioral Cloning Training Program')
+	parser.add_argument('-d', type=str,   help='data directory',   default='data', dest='data_dir')
+	parser.add_argument('-l', type=float, help='learning rate',    default=0.001,  dest='learning_rate')
+	parser.add_argument('-b', type=int,   help='batch size',       default=40,  dest='batch_size')
+	parser.add_argument('-e', type=int,   help='number of epochs', default=10,     dest='epochs')
+
+	args = parser.parse_args()
 	
-	train()
+	train(args)
+
+	# Use torch.jit.trace to generate a torch.jit.ScriptModule via tracing.
+	trained_model = StereoCNN(utils.INPUT_SHAPE, 3, 1)
+	
+	trained_model.load_state_dict(torch.load('trained.pt'))
+
+	example = torch.rand(1, utils.IMAGE_CHANNELS, utils.IMAGE_HEIGHT, utils.IMAGE_WIDTH)
+
+	traced_script_module = torch.jit.trace(trained_model, (example, example))
+	traced_script_module.save('trained_script_module.pt')
